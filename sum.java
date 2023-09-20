797681e8e7b0851b9d8ca233c34c4a9c1bb08d4e
jainex17 <jainexp017@gmail.com>
2023-09-20 12:25:54 +0530

    add sum function

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+    
+    public static int sum(int n,int m){
+        return n+m;        
+    }
+    
+    public static void main(String[] args) {
+        
+        sum(5,5);
+    }
+}
